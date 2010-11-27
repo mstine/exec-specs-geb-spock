e64e984d3be93866ea253fdfc8a62a1f86f7ce71
Matt Stine <matt.stine@gmail.com>
2010-11-27 16:52:23 -0600

    Add naked specs

--- src/test/groovy/com/deepsouthsoftware/execspec/specs/NakedSpec2.groovy
+++ src/test/groovy/com/deepsouthsoftware/execspec/specs/NakedSpec2.groovy
@@ -3,8 +3,8 @@ package com.deepsouthsoftware.execspec.specs
 import geb.spock.GebSpec
 import com.deepsouthsoftware.execspec.pages.*
 
-class FindSpeakerFlowSpec extends GebSpec {
-		
+class NakedSpec2 extends GebSpec {
+	
 	String getBaseUrl() {
 		"http://localhost:8080"
 	}
@@ -12,33 +12,21 @@ class FindSpeakerFlowSpec extends GebSpec {
 	def "test find speaker flow"() {
 		given: "Customer is at the Fluffbox Home Page"
 		to HomePage
-		
+				
 		when: "Customer clicks on 'FIND A SPEAKER'"
 		findASpeaker.click(FindASpeakerPage)
 		
 		and: "Customer clicks on 'Alex Miller'"
-		at FindASpeakerPage
-		alexMiller.link.click(AlexMillerDetailPage)
+		at(FindASpeakerPage)
 		
 		and: "Customer clicks on 'Find this Speaker'"
-		at AlexMillerDetailPage
-		findThisSpeaker.link.click(FindKioskBySpeakerPage)
 		
 		and: "Customer clicks on 'RENT NOW'"
-		at FindKioskBySpeakerPage
-		rentNow.link.click(ReserveSpeakerPage)
 		
 		and: "Customer clicks on 'Continue'"
-		at ReserveSpeakerPage
-		continueToSignInOrCreateAccount.click(LoginPage)
 		
 		and: "Customer enters username, password, and clicks 'Log in'"
-		at LoginPage
-		loginForm.j_username = "joeuser"
-		loginForm.j_password = "password"
-		loginButton.click(PaymentPage)
 		
 		then: "If we'd implemented payment, we'd be ready to pay."
-		at PaymentPage
 	}
 }
--- src/test/groovy/com/deepsouthsoftware/execspec/specs/NakedSpec3.groovy
+++ src/test/groovy/com/deepsouthsoftware/execspec/specs/NakedSpec3.groovy
@@ -3,42 +3,32 @@ package com.deepsouthsoftware.execspec.specs
 import geb.spock.GebSpec
 import com.deepsouthsoftware.execspec.pages.*
 
-class FindSpeakerFlowSpec extends GebSpec {
-		
+class NakedSpec3 extends GebSpec {
+
 	String getBaseUrl() {
 		"http://localhost:8080"
 	}
-	
+
 	def "test find speaker flow"() {
 		given: "Customer is at the Fluffbox Home Page"
 		to HomePage
 		
 		when: "Customer clicks on 'FIND A SPEAKER'"
 		findASpeaker.click(FindASpeakerPage)
-		
+
 		and: "Customer clicks on 'Alex Miller'"
 		at FindASpeakerPage
 		alexMiller.link.click(AlexMillerDetailPage)
-		
+
 		and: "Customer clicks on 'Find this Speaker'"
 		at AlexMillerDetailPage
-		findThisSpeaker.link.click(FindKioskBySpeakerPage)
-		
+
 		and: "Customer clicks on 'RENT NOW'"
-		at FindKioskBySpeakerPage
-		rentNow.link.click(ReserveSpeakerPage)
-		
+
 		and: "Customer clicks on 'Continue'"
-		at ReserveSpeakerPage
-		continueToSignInOrCreateAccount.click(LoginPage)
-		
+
 		and: "Customer enters username, password, and clicks 'Log in'"
-		at LoginPage
-		loginForm.j_username = "joeuser"
-		loginForm.j_password = "password"
-		loginButton.click(PaymentPage)
-		
+
 		then: "If we'd implemented payment, we'd be ready to pay."
-		at PaymentPage
 	}
 }
